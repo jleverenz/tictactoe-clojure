aa37727f1962b5b62357c3c5fa377c00719d7890
Jeff Leverenz <jleveren@wms.com>
2012-11-14 04:33:20 -0600

    Refactored to use src directory

--- src/tictactoe/core.clj
+++ src/tictactoe/core.clj
@@ -1,6 +1,37 @@
 (ns tictactoe.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+;; A Board is represented by a list of 9 positions, 0-based.
+;;    0 1 2
+;;    3 4 5
+;;    6 7 8
+;;
+;; The winning combinations are then:
+;;
+;;    top       0 1 2
+;;    bottom                6 7 8
+;;    left      0     3     6
+;;    right         2     5     8
+;;    vert        1     4     7
+;;    horz            3 4 5
+;;    criss     0       4       8
+;;    cross         2   4   6
+
+(def winning-lines
+  ['(0 1 2) '(6 7 8)'(0 3 6)'(2 5 8)'(1 4 7)'(3 4 5)'(0 4 8)'(2 4 6)])
+
+;; Returns 1 or 2 indicating which player won on LINE of BOARD, or 0 if no
+;; player won
+(defn win-on-line [board line]
+  ;; the non-distinct map will be set to the 'marks' on that row.  Think "X X
+  ;; O" or "X . ." or "O O O".  Except it'll be 0,1,2 values.  Distinct
+  ;; collapses the list.  I win is found when the distinct symbol list for a
+  ;; winline reduces to 1 (X) or 2 (o).
+  (let [marks (distinct (map #(nth board %) line))]
+    (if (= 1 (count marks))
+         (first marks)                  ; only one type, it's XXX or OOO
+         0)))                           ; everything else is a loser
+
+(defn is-board-solved? [board]
+  (not (nil? (first (filter #(not (= 0 %))
+                         (map #(win-on-line board %)
+                              winning-lines))))))
--- test/tictactoe/core_test.clj
+++ test/tictactoe/core_test.clj
@@ -23,55 +23,14 @@
                                          2 1 1
                                          1 1 2 ))
 
-;; top       1 2 3
-;; bottom                7 8 9
-;; left      1     4     7
-;; right         3     6     9
-;; vert        2     5     8
-;; horz            4 5 6
-;; criss     1       5       9
-;; cross         3   5   7
-
-;; ;; draw-board
-;; top       1 2 2
-;; bottom                1 1 2
-;; left      1     2     1
-;; right         2     1     2
-;; vert        2     1     1
-;; horz            2 1 1
-;; criss     1       1       2
-;; cross         2   1   1
-
-(def winning-lines ['(0 1 2)
-                    '(6 7 8)
-                    '(0 3 6)
-                    '(2 5 8)
-                    '(1 4 7)
-                    '(3 4 5)
-                    '(0 4 8)
-                    '(2 4 6)])
-
-
-(defn win-on-line [board line]
-  (let [marks (distinct (map #(nth board %) line))]
-    (if (= 1 (count marks))
-         (first marks)
-         0)))
-
-(defn is-solved? [board]
-
-  (not (nil? (first (filter #(not (= 0 %))
-                         (map #(win-on-line board %)
-                              winning-lines))))))
-
 (deftest test-win-on-line
   (is (= 0 (win-on-line empty-board '(0 1 2))))
   (is (= 1 (win-on-line partially-complete-1wins-board '(0 4 8))))
   (is (= 2 (win-on-line partially-complete-2wins-board '(2 5 8)))))
 
-(deftest test-is-solved?
-  (is (= false (is-solved? empty-board)))
-  (is (= false (is-solved? draw-board)))
-  (is (= true (is-solved? partially-complete-1wins-board)))
-  (is (= true (is-solved? partially-complete-2wins-board)))
+(deftest test-is-board-solved?
+  (is (= false (is-board-solved? empty-board)))
+  (is (= false (is-board-solved? draw-board)))
+  (is (= true (is-board-solved? partially-complete-1wins-board)))
+  (is (= true (is-board-solved? partially-complete-2wins-board)))
   )
